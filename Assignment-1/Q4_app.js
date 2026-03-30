6401d4649a4a316639ec75f141b0ac11699a908d
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-03-30 23:44:54 +0530

    ESM exec of code

---
+++ Assignment-1/Q4_app.js
@@ -0,0 +1,8 @@
+// This question uses ES Modules. To execute, "type": "module" must be added in package.json or run in a separate environment.”
+import {count,increment} from "./Q4_counter.js";
+console.log(count); //0
+increment();
+console.log(count); //1
+
+
+
---
+++ Assignment-1/Q4_counter.js
@@ -0,0 +1,5 @@
+// This question uses ES Modules. To execute, "type": "module" must be added in package.json or run in a separate environment.”
+export let count = 0;
+export function increment(){
+    count++;
+}
