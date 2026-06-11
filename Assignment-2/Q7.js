ee1938e6e81e83240792a55cf38483f2583cce2e
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-06-12 00:38:13 +0530

    custom Promise.all() versions using async await or both

--- Assignment-2/Q7.js
+++ Assignment-2/Q7.js
@@ -1,9 +1,12 @@
 function myPromiseAll(promises){
     return new Promise( (resolve,reject) => {
+        if(!Array.isArray(promises)){
+            return reject(new TypeError("Input must be an array."));
+        }
         if(promises.length == 0){
             return resolve([]);
         }
-        let results = [];
+        const results = [];
         let completed = 0;
         promises.forEach( (p,i) => {
             Promise.resolve(p)
@@ -14,17 +17,21 @@ function myPromiseAll(promises){
                         resolve(results);
                     }
                  })
-                 .catch(reject);
+                 .catch((err) => {
+                    reject(err);
+                 });
         });
     }); 
 }
-const p1 = Promise.resolve("Task 1 done");
-const p2 = new Promise( (resolve) => setTimeout(() => resolve("Task 2 Done (delayed)"),2000)); // delay of 2000ms
-const p3 = 42 // non-promise value , handled by Promise.resolve(p)
-myPromiseAll([p1,p2,p3])
-    .then((results) => {
-        console.log("All results: ",results);
+const p1 = new Promise.resolve(10);
+const p2 = new Promise((resolve) => {
+    setTimeout(() => resolve(20),1000);
+});
+const p3 = 30;
+customPromiseAll([p1,p2,p3])
+    .then((result) => {
+        console.log(result);
     })
     .catch((err) => {
-        console.error("One failed: ",err);
+        console.log(err);    
     });
