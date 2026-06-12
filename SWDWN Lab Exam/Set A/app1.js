715e2ddbf7aa48f54b932744cba15b354958404c
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-06-12 13:56:45 +0530

    import mai file and use a method for addition

---
+++ SWDWN Lab Exam/Set A/app1.js
@@ -0,0 +1,4 @@
+const facultyManager = require("./facultyManager");
+facultyManager.addFaculty("Dr. Roman");
+facultyManager.addFaculty("Dr. Dean");
+console.log("Faculty members added successfully.");
